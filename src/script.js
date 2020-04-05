6728b9451b3470439820d9366ede25d88a0c750a
PauloDeLaMontana <paul.mathon@outlook.fr>
2020-04-06 00:27:37 +0200

    reduce header

--- src/script.js
+++ src/script.js
@@ -1,15 +1,31 @@
-const HEADER_TOGGLING_SIZE = 45;
+const HEADER_TOGGLING_SIZE = 47;
+
+const toggleHeaderBackground = (scrollState) => {
+  document.getElementById("header-background").style.height = `${
+    47 - scrollState
+  }ch`;
+};
 
 const toggleHeaderTitle = (scrollState) => {
-  document.getElementsByClassName('header-background-title-label')[0].style.marginRight = `${scrollState * 2.22}%`;
-  document.getElementsByClassName('header-background-title-label')[1].style.marginLeft = `${scrollState * 2.22}%`;
+  document.getElementsByClassName(
+    "header-background-title-label"
+  )[0].style.marginRight = `${scrollState * 2.13}%`;
+  document.getElementsByClassName(
+    "header-background-title-label"
+  )[1].style.marginLeft = `${scrollState * 2.13}%`;
 };
 
 const toggleHeaderMenu = (scrollState) => {
-  console.log('exe', scrollState, 20 - scrollState);
-  document.getElementById('header-menu-wrapper').style.paddingTop = `${20 - (scrollState / 2.25)}ch`;
-  document.getElementsByClassName('header-menu-item')[1].style.paddingRight = `${scrollState / 4.5}%`;
-  document.getElementsByClassName('header-menu-item')[2].style.marginLeft = `${scrollState / 4.5}%`;
+  console.log("exe", scrollState, 20 - scrollState);
+  document.getElementById("header-menu-wrapper").style.paddingTop = `${
+    20 - scrollState / 2.25
+  }ch`;
+  document.getElementsByClassName(
+    "header-menu-item"
+  )[1].style.paddingRight = `${scrollState / 6.71}%`;
+  document.getElementsByClassName("header-menu-item")[2].style.paddingLeft = `${
+    scrollState / 6.71
+  }%`;
 };
 
 const convertToCh = () => {
@@ -23,22 +39,30 @@ const convertToCh = () => {
 let isToggleHeaderEnd = false;
 const toggleHeader = () => {
   const slowScrollY = convertToCh();
+  console.log("state", slowScrollY);
   if (slowScrollY <= HEADER_TOGGLING_SIZE) {
     isToggleHeaderEnd = false;
-    document.getElementsByTagName('header')[0].style.height = `${75 - slowScrollY}ch`;
+    document.getElementsByTagName("header")[0].style.height = `${
+      75 - slowScrollY
+    }ch`;
+    toggleHeaderBackground(slowScrollY);
     toggleHeaderTitle(slowScrollY);
     toggleHeaderMenu(slowScrollY);
   } else if (isToggleHeaderEnd === false) {
     isToggleHeaderEnd = true;
-    document.getElementsByTagName('header')[0].style.height = '30ch';
-    document.getElementsByClassName('header-background-title-label')[0].style.marginRight = '100%';
-    document.getElementsByClassName('header-background-title-label')[1].style.marginLeft = '100%';
-    document.getElementById('header-menu-wrapper').style.paddingTop = '0';
-    document.getElementById('header-menu-wrapper').style.height = '10ch';
-    // document.getElementById('header-menu-wrapper').style.background = 'linear-gradient(rgb(219, 219, 219), rgb(250, 250, 250))';
-
-    document.getElementsByClassName('header-menu-item')[1].style.paddingRight = '10%';
-    document.getElementsByClassName('header-menu-item')[2].style.marginLeft = '10%';
+    document.getElementsByTagName("header")[0].style.height = "24ch";
+    document.getElementById("header-background").style.height = "16ch";
+    document.getElementsByClassName(
+      "header-background-title-label"
+    )[0].style.marginRight = "100%";
+    document.getElementsByClassName(
+      "header-background-title-label"
+    )[1].style.marginLeft = "100%";
+    document.getElementById("header-menu-wrapper").style.paddingTop = "0";
+    document.getElementsByClassName("header-menu-item")[1].style.paddingRight =
+      "7%";
+    document.getElementsByClassName("header-menu-item")[2].style.paddingLeft =
+      "7%";
   }
 };
 
